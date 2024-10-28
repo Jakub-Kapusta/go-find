77cbecf0fc5f690ad8b8a0ac2bee3f30583384f4
Jakub Kapusta <jakub-dev1@protonmail.com>
2024-10-28 14:48:50 +0900

    Add sqlite boilerplate

--- src/apps/dbupdate/dbupdate.go
+++ src/apps/dbupdate/dbupdate.go
@@ -3,8 +3,26 @@ package dbupdate
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func DbUpdate(ctx context.Context, args []string, rootDir string, isSearchPath bool, searchPath string) {
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+	}
+	defer db.Close()
 
+	var version string
+	err = db.QueryRow("SELECT SQLITE_VERSION()").Scan(&version)
+
+	if err != nil {
+		os.Stderr.WriteString(err.Error() + "\n")
+	}
+
+	fmt.Println(version)
 }
